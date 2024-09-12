95f521c12af6dd03ecac16266a768bc955f7b4c9
juni1173 <junaid1173@gmail.com>
2024-09-12 13:14:53 +0500

    Dashboard Permissions

--- src/views/Helpers/PermissionsHelper.js
+++ src/views/Helpers/PermissionsHelper.js
@@ -3,12 +3,6 @@ import React from 'react'
 const PermissionsHelper = () => {
     const userData = JSON.parse(localStorage.getItem('userData'))
     const user_role = userData.user_role
-    if (user_role === 'admin') {
-      window.location.href = "/admin/dashboard"
-    } else {
-        window.location.href = "/employee/dashboard"
-    
-    }
   return (
         user_role
   )
